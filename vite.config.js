c571d112665ed095f4c2bb738c2c2c65450d234e
rvtkovsk <k3rutkowska@gmail.com>
2024-07-05 22:41:07 +0200

    starting new project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "05_Dzien_3/03_Tworzenie_ukladu_strony/02_Wycieczki_w_kosmos";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
